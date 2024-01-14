ac9d0540ca92ddd3d347873b3aec1786d0032fea
Rohin <git@r0h.in>
2024-01-13 22:11:07 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,13 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         Coming soooooon (dont buy a bed yet).
+        <br /><br />
+        Contacts:<br />
+        <a href="mailto:jacksdorr@gmail.com">Jack (jacksdorr@gmail.com)</a>
+        <a href="mailto:me@r0h.in">Rohin (me@r0h.in)</a>
+        <br /><br />
+        Terms of Service / Privacy Policy / Payments Policy<br />
+        N/A
       </div>
     </main>
   )
