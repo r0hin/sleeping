33dd95a8c04bfa8d7853fd74ad969630d53aeeaa
Rohin <git@r0h.in>
2024-01-18 12:10:06 -0500

    Replace ' with &apos;

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,7 @@ export default function About() {
     <>
         <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl mb-8">About our Company</h1>
         <p className="mb-2">Welcome to our bed rental company, an innovative solution founded by students in London, Ontario. Our mission is to provide affordable and quality bedding solutions for everyone, especially those who need temporary arrangements without the high costs of purchasing.</p>
-        <p>Renting a bed from us is not only cost-effective but also ensures you get a brand new, never slept on, 5-star rated mattress. We believe in quality, and that's why each bed set, inclusive of the mattress and frame, is valued at around $2,000, ensuring you the best sleep experience.</p>
+        <p>Renting a bed from us is not only cost-effective but also ensures you get a brand new, never slept on, 5-star rated mattress. We believe in quality, and that&apos;s why each bed set, inclusive of the mattress and frame, is valued at around $2,000, ensuring you the best sleep experience.</p>
     </> 
   )
 }
