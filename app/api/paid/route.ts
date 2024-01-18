5a890dc277441185e612973434ecca4cdbcadd78
Rohin <git@r0h.in>
2024-01-17 22:29:12 -0500

    Packages

--- app/api/paid/route.ts
+++ app/api/paid/route.ts
@@ -1,4 +1,26 @@
-export interface StripeEvent {
+export const runtime = 'edge'; // 'nodejs' is the default
+export const dynamic = 'force-dynamic'; // static by default, unless reading the request
+
+import { Resend } from 'resend';
+
+export async function POST(request: Request) {
+  const body = await request.json() as StripeEvent;
+  const resend = new Resend(process.env.RESEND_KEY || "");
+  if (body.data.object.metadata.password !== "30nn") {
+    return new Response("OK");
+  }
+
+  await resend.emails.send({
+    from: 'Scholar Snooze <purchases@scholarsnooze.com>',
+    to: ['me@r0h.in'],
+    subject: 'NEW MONEY BABY!',
+    text: `New order. ${JSON.stringify(body.data.object.metadata)}`,
+  });
+
+  return new Response("OK");
+}
+
+interface StripeEvent {
   id: string,
   object: string,
   api_version: string,
