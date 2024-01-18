1347e84031b734e788916f1e2bd5375590ac826a
Rohin <git@r0h.in>
2024-01-17 19:39:48 -0500

    Webhooks

---
+++ app/api/paid/route.ts
@@ -0,0 +1,20 @@
+export const runtime = 'edge'; // 'nodejs' is the default
+export const dynamic = 'force-dynamic'; // static by default, unless reading the request
+
+import stripe from "stripe";
+
+export async function POST(request: Request) {
+  const signature = request.headers.get("stripe-signature") || "";
+  const client = new stripe(process.env.STRIPE_KEY || "");
+  const body = await request.json();
+  let event;
+  try {
+    event = stripe.webhooks.constructEvent(body, signature, process.env.STRIPE_WEBHOOK_KEY || "");
+  } catch (err: any) {
+    return new Response(`Webhook Error: ${err.message || ""}`, { status: 400 });
+  }
+
+  console.log(event)
+
+  return new Response(JSON.stringify({ received: true }));
+}
