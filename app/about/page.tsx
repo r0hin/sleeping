b2e3a4c176d00db101d11dba52c10980e0f9f3e0
Jack <138183523+jacksdorr@users.noreply.github.com>
2024-01-18 11:15:37 -0500

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,10 @@
 export default function About() {
   return (
-    <h1>About!</h1>
-  )
+   <main>
+        <section>
+            <h1>About Our Company</h1>
+            <p>Welcome to our bed rental company, an innovative solution founded by students in London, Ontario. Our mission is to provide affordable and quality bedding solutions for everyone, especially those who need temporary arrangements without the high costs of purchasing.</p>
+            <p>Renting a bed from us is not only cost-effective but also ensures you get a brand new, never slept on, 5-star rated mattress. We believe in quality, and that's why each bed set, inclusive of the mattress and frame, is valued at around $2,000, ensuring you the best sleep experience.</p>
+        </section>
+    </main> )
 }
